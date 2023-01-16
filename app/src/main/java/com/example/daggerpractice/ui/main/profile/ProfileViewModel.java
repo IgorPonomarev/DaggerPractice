55b19c8f0a8a8f288914a3867092c3a67c44ea2d
Igor Ponomarev <dviss1510@gmail.com>
2023-01-16 18:38:19 +0400

    Added SessionManager to ProfileViewModel Display user data in ProfileFragment

--- app/src/main/java/com/example/daggerpractice/ui/main/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/daggerpractice/ui/main/profile/ProfileViewModel.java
@@ -2,7 +2,12 @@ package com.example.daggerpractice.ui.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.daggerpractice.SessionManager;
+import com.example.daggerpractice.models.User;
+import com.example.daggerpractice.ui.auth.AuthResource;
 
 import javax.inject.Inject;
 
@@ -10,8 +15,15 @@ public class ProfileViewModel extends ViewModel {
 
     private static final String TAG = "ProfileViewModel";
 
+    private final SessionManager sessionManager;
+
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         Log.d(TAG, "ProfileViewModel: ViewModel is injected");
+    }
+
+    public LiveData<AuthResource<User>> getAuthenticatedUser() {
+        return sessionManager.getAuthUser();
     }
 }
