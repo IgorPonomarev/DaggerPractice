274807d66a3326f1ad048b1ac4f099e811d3e7c5
Igor Ponomarev <dviss1510@gmail.com>
2023-01-16 16:22:21 +0400

    Added logout button and made it navigate to AuthActivity

--- app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
+++ app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.daggerpractice.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.daggerpractice.BaseActivity;
@@ -18,5 +22,24 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.logout: {
+                sessionManager.logout();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
