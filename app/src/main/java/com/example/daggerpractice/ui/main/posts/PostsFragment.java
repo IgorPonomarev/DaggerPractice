2c1f78158d5e74caf6a7858446eac4dab54d19d2
Igor Ponomarev <dviss1510@gmail.com>
2023-01-17 13:34:06 +0400

    Observe posts in PostsViewModel

--- app/src/main/java/com/example/daggerpractice/ui/main/posts/PostsFragment.java
+++ app/src/main/java/com/example/daggerpractice/ui/main/posts/PostsFragment.java
@@ -1,6 +1,7 @@
 package com.example.daggerpractice.ui.main.posts;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +9,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.daggerpractice.R;
+import com.example.daggerpractice.models.Post;
+import com.example.daggerpractice.ui.main.Resource;
 import com.example.daggerpractice.ui.main.profile.ProfileViewModel;
 import com.example.daggerpractice.viewmodels.ViewModelProviderFactory;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -40,5 +46,19 @@ public class PostsFragment extends DaggerFragment {
         recyclerView = view.findViewById(R.id.recycler_view);
 
         viewModel = new ViewModelProvider(this, providerFactory).get(PostsViewModel.class);
+
+        subscribeObservers();
+    }
+
+    private void subscribeObservers() {
+        viewModel.observePosts().removeObservers(getViewLifecycleOwner());
+        viewModel.observePosts().observe(getViewLifecycleOwner(), new Observer<Resource<List<Post>>>() {
+            @Override
+            public void onChanged(Resource<List<Post>> listResource) {
+                if (listResource != null) {
+                    Log.d(TAG, "onChanged: " + listResource.data);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/daggerpractice/ui/main/posts/PostsViewModel.java
+++ app/src/main/java/com/example/daggerpractice/ui/main/posts/PostsViewModel.java
@@ -2,12 +2,24 @@ package com.example.daggerpractice.ui.main.posts;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.example.daggerpractice.SessionManager;
+import com.example.daggerpractice.models.Post;
 import com.example.daggerpractice.network.main.MainAPI;
+import com.example.daggerpractice.ui.main.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
+
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class PostsViewModel extends ViewModel {
 
@@ -17,10 +29,59 @@ public class PostsViewModel extends ViewModel {
     private final SessionManager sessionManager;
     private final MainAPI mainAPI;
 
+    private MediatorLiveData<Resource<List<Post>>> posts;
+
     @Inject
     public PostsViewModel(SessionManager sessionManager, MainAPI mainAPI) {
         this.sessionManager = sessionManager;
         this.mainAPI = mainAPI;
         Log.d(TAG, "PostsViewModel: viewmodel is working");
+    }
+
+    public LiveData<Resource<List<Post>>> observePosts() {
+        if (posts == null) {
+            posts = new MediatorLiveData<>();
+            posts.setValue(Resource.loading(null));
+
+            //in real app better to create a livedata for AuthUser
+            final LiveData<Resource<List<Post>>> source = LiveDataReactiveStreams.fromPublisher(
+                    mainAPI.getUsersPosts(sessionManager.getAuthUser().getValue().data.getId())
+                            .onErrorReturn(new Function<Throwable, List<Post>>() {
+                                @Override
+                                public List<Post> apply(Throwable throwable) throws Throwable {
+                                    Log.e(TAG, "apply: ", throwable);
+                                    Post post = new Post();
+                                    post.setId(-1);
+                                    ArrayList<Post> posts = new ArrayList<>();
+                                    posts.add(post);
+                                    return posts;
+                                }
+                            })
+
+                            .map(new Function<List<Post>, Resource<List<Post>>>() {
+                                @Override
+                                public Resource<List<Post>> apply(List<Post> posts) throws Throwable {
+                                    if (posts.size() > 0) {
+                                        if (posts.get(0).getId() == -1) {
+                                            return Resource.error("Smth went wrong", null);
+                                        }
+                                    }
+                                    return Resource.success(posts);
+                                }
+                            })
+
+                            .subscribeOn(Schedulers.io())
+            );
+            posts.addSource(source, new Observer<Resource<List<Post>>>() {
+                @Override
+                public void onChanged(Resource<List<Post>> listResource) {
+                    posts.setValue(listResource);
+                    posts.removeSource(source);
+                }
+            });
+        }
+
+        return posts;
+
     }
 }
