60b71b3a3da10e29149281249840f5b087e6d14d
Igor Ponomarev <dviss1510@gmail.com>
2023-01-17 14:01:28 +0400

    Inflate RecyclerView

--- app/src/main/java/com/example/daggerpractice/di/main/MainModule.java
+++ app/src/main/java/com/example/daggerpractice/di/main/MainModule.java
@@ -1,6 +1,7 @@
 package com.example.daggerpractice.di.main;
 
 import com.example.daggerpractice.network.main.MainAPI;
+import com.example.daggerpractice.ui.main.posts.PostsRecyclerAdapter;
 
 import dagger.Module;
 import dagger.Provides;
@@ -8,6 +9,11 @@ import retrofit2.Retrofit;
 
 @Module
 public class MainModule {
+
+    @Provides
+    static PostsRecyclerAdapter provideAdapter() {
+        return new PostsRecyclerAdapter();
+    }
 
     @Provides
     static MainAPI provideMainAPI(Retrofit retrofit) {
--- app/src/main/java/com/example/daggerpractice/ui/main/posts/PostsFragment.java
+++ app/src/main/java/com/example/daggerpractice/ui/main/posts/PostsFragment.java
@@ -11,12 +11,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.daggerpractice.R;
 import com.example.daggerpractice.models.Post;
 import com.example.daggerpractice.ui.main.Resource;
 import com.example.daggerpractice.ui.main.profile.ProfileViewModel;
+import com.example.daggerpractice.util.VerticalSpacingItemDecoration;
 import com.example.daggerpractice.viewmodels.ViewModelProviderFactory;
 
 import java.util.List;
@@ -33,6 +35,9 @@ public class PostsFragment extends DaggerFragment {
     private RecyclerView recyclerView;
 
     @Inject
+    PostsRecyclerAdapter adapter;
+
+    @Inject
     ViewModelProviderFactory providerFactory;
 
     @Nullable
@@ -47,6 +52,7 @@ public class PostsFragment extends DaggerFragment {
 
         viewModel = new ViewModelProvider(this, providerFactory).get(PostsViewModel.class);
 
+        initRecyclerView();
         subscribeObservers();
     }
 
@@ -56,9 +62,30 @@ public class PostsFragment extends DaggerFragment {
             @Override
             public void onChanged(Resource<List<Post>> listResource) {
                 if (listResource != null) {
-                    Log.d(TAG, "onChanged: " + listResource.data);
+                    switch (listResource.status) {
+                        case LOADING: {
+                            Log.d(TAG, "onChanged: Loading");
+                            break;
+                        }
+                        case SUCCESS: {
+                            Log.d(TAG, "onChanged: got posts");
+                            adapter.setPosts(listResource.data);
+                            break;
+                        }
+                        case ERROR: {
+                            Log.e(TAG, "onChanged: error" + listResource.message);
+                            break;
+                        }
+                    }
                 }
             }
         });
+    }
+
+    private void initRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        VerticalSpacingItemDecoration itemDecoration = new VerticalSpacingItemDecoration(15);
+        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.setAdapter(adapter);
     }
 }
